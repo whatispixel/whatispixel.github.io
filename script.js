462d0f110e97871426be101ba9a38be3c2d02aee
whatispixel <amankdhoor+pixel@gmail.com>
2017-01-21 22:41:11 -0800

    navigation and new page work as a unit

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
    $(document).ready(function(){
 
   	$('.closetab').hide();
+    
+$('#front').hide();
 
     $('.cube').addClass('animation');
   $('.currentPage').hide();
+
   var navigation = '.cube div';
 
    $(navigation).on('click', function(){
@@ -13,11 +16,15 @@
       var page=this.className;
       console.log(page);
       $('.currentPage').show().addClass('black');
-       
+                   
+      
       
 
   });
 
 
-   
+   $('#close').on('click', function(){
+      $('.currentPage').hide();
+      $('.cube').show().addClass('animation');  
+   });    
   });
